e2050fb7b2a949f5a94967796a9ec8530c33d093
SadiqueShoaib <136719576+Sadiqsh-2001@users.noreply.github.com>
2026-03-15 03:49:44 +0530

    Remove old Business_queries file

--- SQL_queries/Business_queries.sql
+++
@@ -1,5 +0,0 @@
-Select * from orders_enriched;
-Select carrier,count(order_id)as total_orders,sum(is_sla_breached) as total_sla_breaches,ROUND(100.00 * sum(is_sla_breached)/count(order_id),2) as breach_percentage
-from orders_enriched
-group by carrier
-order by breach_percentage desc;
